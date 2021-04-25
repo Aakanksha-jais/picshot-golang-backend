52314667eed0b503712b71a68a313a46098b966e
Aakanksha <jaiswal14aakanksha@gmail.com>
2021-04-25 15:56:45 +0530

    ISSUE #6 | add basic custom errors

---
+++ errors/invalid_param.go
@@ -0,0 +1,11 @@
+package errors
+
+import "fmt"
+
+type InvalidParam struct {
+	Param string `json:"param"`
+}
+
+func (e InvalidParam) Error() string {
+	return fmt.Sprintf("invalid value for param %s", e.Param)
+}
