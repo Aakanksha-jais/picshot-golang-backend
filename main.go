dbae4230e64a706f9c1d224afdd7344f43b2d260
Aakanksha <jaiswal14aakanksha@gmail.com>
2021-05-02 14:02:02 +0530

    ISSUE #10 | add skeleton implementation for main.go

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Aakanksha-jais/picshot-golang-backend/driver"
+	handlerAccount "github.com/Aakanksha-jais/picshot-golang-backend/handlers/account"
+	handlerBlog "github.com/Aakanksha-jais/picshot-golang-backend/handlers/blog"
+	"github.com/Aakanksha-jais/picshot-golang-backend/middlewares"
+	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/configs"
+	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/log"
+	serviceAccount "github.com/Aakanksha-jais/picshot-golang-backend/services/account"
+	serviceBlog "github.com/Aakanksha-jais/picshot-golang-backend/services/blog"
+	storeAccount "github.com/Aakanksha-jais/picshot-golang-backend/stores/account"
+	storeBlog "github.com/Aakanksha-jais/picshot-golang-backend/stores/blog"
+	storeTag "github.com/Aakanksha-jais/picshot-golang-backend/stores/tag"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+
+	logger := log.NewLogger()
+	configLoader := configs.NewConfigLoader("/configs", logger)
+
+	mongoDB, err := driver.NewMongoConfigs(configLoader).ConnectToMongo()
+	if err != nil {
+		logger.Fatalf("cannot connect to  mongo %v", err)
+		return
+	}
+
+	sqlDB, err := driver.NewSQLConfigs(configLoader).ConnectToSQL()
+	if err != nil {
+		logger.Fatalf("cannot connect to sql %v", err)
+		return
+	}
+
+	// Dependency Injection
+	blogStore := storeBlog.New(mongoDB, logger)
+	tagStore := storeTag.New(mongoDB, logger)
+	accountStore := storeAccount.New(sqlDB, logger)
+
+	blogService := serviceBlog.New(blogStore, tagStore, logger)
+	accountService := serviceAccount.New(accountStore, blogService, logger)
+
+	blogHandler := handlerBlog.New(blogService, logger)
+	accountHandler := handlerAccount.New(accountService, logger)
+
+	r := mux.NewRouter()
+
+	// Routes
+	r.HandleFunc("/login", accountHandler.LogIn)
+	r.HandleFunc("/signup", accountHandler.SignUp)
+	r.HandleFunc("/blogs", blogHandler.GetAll)
+
+	// Middlewares
+	r.Use(middlewares.Authentication)
+
+	server := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf("localhost:%s", configLoader.Get("HTTP_PORT")),
+	}
+
+	logger.Fatal(server.ListenAndServe())
+}
