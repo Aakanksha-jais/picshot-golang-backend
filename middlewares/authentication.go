fe589b131fd08cb285638848437da1e0ca738375
Aakanksha <jaiswal14aakanksha@gmail.com>
2021-06-15 22:50:13 +0530

    ISSUE #20 | modify middleware

--- middlewares/authentication.go
+++ middlewares/authentication.go
@@ -5,52 +5,59 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/app/response"
+	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/errors"
+
 	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/auth"
 	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/log"
 )
 
-const headerLength = 2
+func Authentication(logger log.Logger, options auth.Options) func(inner http.Handler) http.Handler {
+	oAuth := auth.New(options)
 
-func Authentication(logger log.Logger, claims auth.Claims) func(inner http.Handler) http.Handler {
 	return func(inner http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			logger.Debugf("request on endpoint: %s", r.URL.String())
 
-			if exemptPath(r) {
+			if err, ok := exemptPath(oAuth, r); ok {
+				if err != nil {
+					response.WriteResponse(w, err, nil, logger)
+					return
+				}
+
 				inner.ServeHTTP(w, r)
 				return
 			}
 
-			authHeader := r.Header.Get("Authorization")
-			fields := strings.Fields(authHeader)
+			token, err := oAuth.Validate(r)
+			if err == nil {
+				claims := token.Claims.(*auth.Claims)
 
-			if len(fields) != headerLength {
-				logger.Errorf("cannot fetch auth-token (invalid auth-header): %v", fields)
-				w.WriteHeader(http.StatusUnauthorized)
+				jwtIDKey := auth.JWTContextKey("claims")
+				r = r.WithContext(context.WithValue(r.Context(), jwtIDKey, claims))
 
+				logger.Debugf("user_id: %v authorized to make request on %v.", r.Context().Value(jwtIDKey).(*auth.Claims).UserID, r.URL.Path)
+
+				inner.ServeHTTP(w, r)
 				return
 			}
 
-			jwtToken := fields[1]
-
-			err := claims.ParseToken(jwtToken)
-			if err != nil {
-				logger.Error(err)
-				w.WriteHeader(http.StatusUnauthorized)
-
-				return
-			}
-
-			jwtIDKey := auth.JWTContextKey("user_id")
-			r = r.WithContext(context.WithValue(r.Context(), jwtIDKey, claims.GetUserID()))
-			logger.Debugf("user_id: %v authorized to make request on %v.", r.Context().Value(jwtIDKey), r.URL.Path)
-
-			inner.ServeHTTP(w, r)
+			response.WriteResponse(w, err, nil, logger)
 		})
 	}
 }
 
-func exemptPath(req *http.Request) bool {
+func exemptPath(oAuth *auth.OAuth, req *http.Request) (error, bool) {
 	url := req.URL.Path
-	return strings.HasSuffix(url, "/login") || strings.HasSuffix(url, "/signup") || strings.HasSuffix(url, "/available")
+
+	if strings.HasSuffix(url, "/login") || strings.HasSuffix(url, "/signup") {
+		_, err := oAuth.Validate(req)
+		if err != nil {
+			return nil, true // login and signup allowed if auth header is invalid
+		}
+
+		return errors.Error{Type: "login-error", Msg: "logout before logging in"}, false
+	}
+
+	return nil, strings.HasSuffix(url, "/available")
 }
