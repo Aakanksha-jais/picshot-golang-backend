727609f92441f03654fc548bc2fbf982790b9377
Aakanksha <jaiswal14aakanksha@gmail.com>
2021-06-15 22:58:33 +0530

    ISSUE #20 | modify server and context in accordance with new auth

--- pkg/app/context.go
+++ pkg/app/context.go
@@ -2,27 +2,20 @@ package app
 
 import (
 	"context"
+	err "errors"
 	"fmt"
 	"net/http"
-	"strings"
-
-	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/auth"
-
-	"github.com/Aakanksha-jais/picshot-golang-backend/pkg/errors"
-
-	err "errors"
 )
 
 type Context struct {
 	context.Context
 	Request *Request
 	w       http.ResponseWriter
-	auth.Claims
 	*App
 }
 
 func NewContext(r *Request, w http.ResponseWriter, app *App) *Context {
-	return &Context{Request: r, w: w, App: app, Claims: auth.NewEmptyClaim()}
+	return &Context{Request: r, w: w, App: app}
 }
 
 func (c *Context) reset(r *Request, w http.ResponseWriter) {
@@ -40,19 +33,4 @@ func (c *Context) SetStatus(status int) error {
 	c.w.WriteHeader(status)
 
 	return err.New("do not set header")
-}
-
-func (c *Context) CheckAuthHeader() error {
-	authHeader := c.Request.Header("Authorization")
-	if authHeader == "" || len(strings.Split(authHeader, " ")) < 2 {
-		return nil
-	}
-
-	jwtToken := strings.Split(authHeader, " ")[1]
-	e := c.ParseToken(jwtToken)
-	if e != nil {
-		return nil
-	}
-
-	return errors.Error{Type: "login-error", Message: "logout before logging in"}
 }
