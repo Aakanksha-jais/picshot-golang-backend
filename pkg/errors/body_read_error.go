11f1b76afe4e2b8734f2153a7d5e56f069efef44
Aakanksha <jaiswal14aakanksha@gmail.com>
2021-06-15 23:00:27 +0530

    modify errors package to introduce more flexibility in error msg/description

--- pkg/errors/body_read_error.go
+++ pkg/errors/body_read_error.go
@@ -4,8 +4,13 @@ import "fmt"
 
 type BodyRead struct {
 	Err error
+	Msg string
 }
 
 func (e BodyRead) Error() string {
-	return fmt.Sprintf("error in reading request body: %s", e.Err.Error())
+	if e.Msg == "" {
+		return fmt.Sprintf("error in reading request body: %s", e.Err.Error())
+	}
+
+	return fmt.Sprintf("%s: %s", e.Msg, e.Err.Error())
 }
--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Error struct {
-	Type    string
-	Message string
-	Err     error
+	Type string
+	Msg  string
+	Err  error
 }
 
 func (e Error) Error() string {
 	if e.Err == nil {
-		return e.Message
+		return e.Msg
 	}
 
-	return fmt.Sprintf("%v; %s", e.Err, e.Message)
+	return fmt.Sprintf("%v; %s", e.Err, e.Msg)
 }
